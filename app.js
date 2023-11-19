fc3c3c0e76b8d42a80e499f9d8d67dc0a8a041ce
Scott Carl <scarl@Scotts-MacBook-Pro.local>
2023-11-19 12:11:36 -0800

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const http = require('http');
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end("<h1>Hello World!</h1>");
+    }).listen(process.env.PORT);
+    
+console.log('App is running...');
